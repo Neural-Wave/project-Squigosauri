c5cbde9177b092a7fa8d46b369c2b3fb300e5a2d
Michele Damian <39302292+damiam@users.noreply.github.com>
2024-10-26 16:59:03 +0200

    remove next

--- src/lib/proomp/prompts.ts
+++ src/lib/proomp/prompts.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import type { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
 
