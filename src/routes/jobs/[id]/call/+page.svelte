9482f1ecd54f82d12ffb1902d3f9543a6dc96925
Matteo Romano <3romano.matteo3@gmail.com>
2024-10-27 01:26:07 +0200

    roba

---
+++ src/routes/jobs/[id]/call/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let conversationState = $state([]);
+	type phraseData = {
+		role: string;
+		skill: string;
+		type: string; // greetings, diploma...
+		textPhrase: string;
+	};
+
+	// function pushPhrase(phraseData) {
+	// 	conversationState.push[phraseData];
+	// }
+</script>
