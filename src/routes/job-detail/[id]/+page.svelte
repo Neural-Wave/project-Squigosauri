f709b7a08edde174747d51a8095977780629f390
Michele Damian <39302292+damiam@users.noreply.github.com>
2024-10-26 19:52:59 +0200

    lint

--- src/routes/job-detail/[id]/+page.svelte
+++ src/routes/job-detail/[id]/+page.svelte
@@ -28,67 +28,70 @@
 			if (res.exists()) {
 				interviewData = res.data() as InterviewData;
 				htmlDescription = marked(interviewData ? interviewData.text : '') as string;
-			} 
+			} else {
+			}
 		});
 	});
-
 </script>
+
 {#if interviewData}
-<Container>
-	<div class="bordershadow-md mx-auto max-w-3xl rounded-lg pl-4 pr-4">
-		<h1 class="mb-2 text-2xl font-bold text-orange-600">
-			{interviewData.jobTitle} - {interviewData.company}
-		</h1>
+	<Container>
+		<div class="bordershadow-md mx-auto max-w-3xl rounded-lg pl-4 pr-4">
+			<h1 class="mb-2 text-2xl font-bold text-orange-600">
+				{interviewData.jobTitle} - {interviewData.company}
+			</h1>
 
-		<div class="mt-8 flex flex-col justify-between gap-4 md:flex-row md:gap-0">
-			<div class="flex flex-col">
-				<h2 class="text-xl font-semibold">Summary</h2>
-				<p class="text-gray-600">
-					Company: <span class="font-semibold">{interviewData.company}</span>
-				</p>
-				<p class="text-gray-600">
-					Salary: <span class="font-semibold">{interviewData.salary}</span>
-				</p>
-				<p class="text-gray-600">
-					Start Date: <span class="font-semibold">{interviewData.startDate}</span>
-				</p>
-				<p class="text-gray-600">
-					Location: <span class="font-semibold">{interviewData.location}</span>
-				</p>
-				<p class="text-gray-600">
-					Seniority: <span class="font-semibold">{interviewData.seniority}</span>
-				</p>
-			</div>
+			<div class="mt-8 flex flex-col justify-between gap-4 md:flex-row md:gap-0">
+				<div class="flex flex-col">
+					<h2 class="text-xl font-semibold">Summary</h2>
+					<p class="text-gray-600">
+						Company: <span class="font-semibold">{interviewData.company}</span>
+					</p>
+					<p class="text-gray-600">
+						Salary: <span class="font-semibold">{interviewData.salary}</span>
+					</p>
+					<p class="text-gray-600">
+						Start Date: <span class="font-semibold">{interviewData.startDate}</span>
+					</p>
+					<p class="text-gray-600">
+						Location: <span class="font-semibold">{interviewData.location}</span>
+					</p>
+					<p class="text-gray-600">
+						Seniority: <span class="font-semibold">{interviewData.seniority}</span>
+					</p>
+				</div>
 
-			<div>
-				<h2 class="text-xl font-semibold">Soft Skills</h2>
-				<ul class="list-disc pl-5">
-					{#each interviewData.softSkills as skill}
-						<li class="text-gray-600">{skill}</li>
-					{/each}
-				</ul>
-			</div>
+				<div>
+					<h2 class="text-xl font-semibold">Soft Skills</h2>
+					<ul class="list-disc pl-5">
+						{#each interviewData.softSkills as skill}
+							<li class="text-gray-600">{skill}</li>
+						{/each}
+					</ul>
+				</div>
 
-			<div>
-				<h2 class="text-xl font-semibold">Hard Skills</h2>
-				<ul class="list-disc pl-5">
-					{#each interviewData.hardSkills as skill}
-						<li class="text-gray-600">{skill}</li>
-					{/each}
-				</ul>
+				<div>
+					<h2 class="text-xl font-semibold">Hard Skills</h2>
+					<ul class="list-disc pl-5">
+						{#each interviewData.hardSkills as skill}
+							<li class="text-gray-600">{skill}</li>
+						{/each}
+					</ul>
+				</div>
 			</div>
+			<hr class="mb-6 mt-6" />
+			<div class="mb-24">
+				<h2 class="text-xl font-semibold">Job description</h2>
+				<div class="rendered-markdown">{@html htmlDescription}</div>
+			</div>
+			<button
+				class="fixed bottom-0 left-1/2 mb-6 flex w-full max-w-xl -translate-x-1/2 items-center justify-center gap-4 rounded-md bg-orange-600 py-2 font-semibold text-white shadow-lg hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+			>
+				<Icon src={BiSolidPhoneCall}></Icon>
+				Start Screening Call
+			</button>
 		</div>
-		<hr class="mb-6 mt-6" />
-		<div class="mb-24">
-			<h2 class="text-xl font-semibold">Job description</h2>
-			<div class="rendered-markdown">{@html htmlDescription}</div>
-		</div>
-		<button
-			class="fixed bottom-0 left-1/2 mb-6 flex w-full max-w-xl -translate-x-1/2 items-center justify-center gap-4 rounded-md bg-orange-600 py-2 font-semibold text-white shadow-lg hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
-		>
-			<Icon src={BiSolidPhoneCall}></Icon>
-			Start Screening Call
-		</button>
-	</div>
-</Container>
+	</Container>
+{:else}
+	<p>nothing to see here </p>
 {/if}
